9f89f6a229ccc2ca6cd43eeca230f4bee7c86231
abubaker <mohammad_abubaker@yahoo.com>
2021-09-15 16:30:45 +0500

    Working...

---
+++ app/src/main/java/org/abubaker/weatherapp/models/WeatherResponse.kt
@@ -0,0 +1,19 @@
+package org.abubaker.weatherapp.models
+
+import java.io.Serializable
+
+// A data model class for using it for the api response. And also create all the models used in this model class.
+data class WeatherResponse(
+    val coord: Coord,
+    val weather: List<Weather>,
+    val base: String,
+    val main: Main,
+    val visibility: Int,
+    val wind: Wind,
+    val clouds: Clouds,
+    val dt: Int,
+    val sys: Sys,
+    val id: Int,
+    val name: String,
+    val cod: Int
+) : Serializable
