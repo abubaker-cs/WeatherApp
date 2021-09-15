62d74f1203c5b32a8daa98e134b081aaa9a4a22e
abubaker <mohammad_abubaker@yahoo.com>
2021-09-15 10:41:56 +0500

    Working...

--- app/src/main/java/org/abubaker/weatherapp/Constants.kt
+++ app/src/main/java/org/abubaker/weatherapp/Constants.kt
@@ -16,31 +16,36 @@ object Constants {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        //
+        // For SDK > 23
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-            //
+            // If we have an active network
             val network = connectivityManager.activeNetwork ?: return false
 
-            //
+            // Get information about the NETWORK availability
             val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
 
 
+            // Inform the user which TYPE of connection is available
             return when {
-                //
+
+                // WIFI
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
 
-                //
+                // SIM DATA Connection
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
 
-                //for other device how are able to connect with Ethernet
+                // Ethernet
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
 
+                // No Network Available
                 else -> false
+
             }
 
         } else {
 
+            // For SDK < 23
             // Returns details about the currently active default data network.
             val networkInfo = connectivityManager.activeNetworkInfo
             return networkInfo != null && networkInfo.isConnectedOrConnecting
