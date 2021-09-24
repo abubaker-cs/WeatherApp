5c622ccd073e942bedc5120028c0d0b01e595201
abubaker <mohammad_abubaker@yahoo.com>
2021-09-24 12:46:26 +0500

    Working...

--- app/src/main/java/org/abubaker/weatherapp/activities/MainActivity.kt
+++ app/src/main/java/org/abubaker/weatherapp/activities/MainActivity.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -103,6 +105,22 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return trueK
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_refresh -> {
+                getLocationWeatherDetails()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     /**
